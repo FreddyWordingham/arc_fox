db118e6ef997cee827cb719260450082084f784a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-08 21:43:34 +0100

    Updated gitignore and added library settings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! Arc library
+
+#![allow(dead_code)]
+#![allow(clippy::all)]
+#![allow(unknown_lints)]
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
+/// Simple test module!
 #[cfg(test)]
 mod tests {
     #[test]
