31d1052facfe2622637497f7e19f32f7e741b4be
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-11 20:26:51 +0100

    Completed geom cube implementation.

--- src/geom/cube.rs
+++ src/geom/cube.rs
@@ -1,7 +1,7 @@
 //! Geometric cube stucture.
 
 use contracts::pre;
-use nalgebra::Point3;
+use nalgebra::{Point3, Vector3};
 
 /// An axis-aligned box.
 /// Commonly used to partition the domain.
@@ -29,6 +29,32 @@ impl Cube {
     pub fn maxs(&self) -> &Point3<f64> {
         &self.maxs
     }
+
+    /// Calculate the widths.
+    pub fn widths(&self) -> Vector3<f64> {
+        self.maxs - self.mins
+    }
+
+    /// Calculate the half-widths.
+    pub fn half_widths(&self) -> Vector3<f64> {
+        (self.maxs - self.mins) / 2.0
+    }
+
+    /// Calculate the centre position.
+    pub fn centre(&self) -> Point3<f64> {
+        nalgebra::center(&self.mins, &self.maxs)
+    }
+
+    /// Determine if the point is contained.
+    /// Points lying exactly at the surface are considered contained.
+    pub fn contained(&self, point: Point3<f64>) -> bool {
+        (self.mins.x <= point.x)
+            && (point.x <= self.maxs.x)
+            && (self.mins.y <= point.y)
+            && (point.y <= self.maxs.y)
+            && (self.mins.z <= point.z)
+            && (point.z <= self.maxs.z)
+    }
 }
 
 #[cfg(test)]
@@ -55,5 +81,17 @@ mod tests {
 
         assert_eq!(cube.mins(), &Point3::new(-1.0, 2.0, -3.14159));
         assert_eq!(cube.maxs(), &Point3::new(1.0, 4.0, 3.14159));
+    }
+
+    #[test]
+    fn calculators() {
+        let cube = Cube::new(
+            Point3::new(-1.0, 2.0, -3.14159),
+            Point3::new(1.0, 4.0, 3.14159),
+        );
+
+        assert_eq!(cube.widths(), Vector3::new(2.0, 2.0, 6.28318));
+        assert_eq!(cube.half_widths(), Vector3::new(1.0, 1.0, 3.14159));
+        assert_eq!(cube.centre(), Point3::new(0.0, 3.0, 0.0));
     }
 }
