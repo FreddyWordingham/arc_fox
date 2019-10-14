ab87b9bd23c1670622ef562b02073b37604d9397
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 19:44:24 +0100

    Increased matcher model complexity.

--- src/bin/matcher.rs
+++ src/bin/matcher.rs
@@ -12,5 +12,20 @@ use arc::phy::{kin::Properties as KinProp, opt::Properties as OptProp, Material}
 fn main() {
     println!("Hello world!");
 
-    let _stratum_corneum = Material::new(Some(OptProp::new(1.0, 0.1)), Some(KinProp::new(1.0)));
+    let _stratum_corneum = Material::new(
+        Some(OptProp::new(1.5, 100.0 * 1e3, 0.630 * 1e3, 0.86)),
+        Some(KinProp::new(1.0)),
+    );
+    let _living_epidermis =
+        Material::new(Some(OptProp::new(1.34, 45.0 * 1e3, 0.872 * 1e3, 0.8)), None);
+    let _papillary_dermis =
+        Material::new(Some(OptProp::new(1.4, 30.0 * 1e3, 0.106 * 1e3, 0.8)), None);
+    let _upper_blood_net_dermis =
+        Material::new(Some(OptProp::new(1.39, 35.0 * 1e3, 0.142 * 1e3, 0.8)), None);
+    let _reticular_dermis =
+        Material::new(Some(OptProp::new(1.4, 25.0 * 1e3, 0.106 * 1e3, 0.8)), None);
+    let _deep_blood_net_dermis =
+        Material::new(Some(OptProp::new(1.38, 30.0 * 1e3, 0.164 * 1e3, 0.8)), None);
+    let _subcutaneous_fat =
+        Material::new(Some(OptProp::new(1.44, 5.0 * 1e3, 0.104 * 1e3, 0.8)), None);
 }
--- src/phy/opt/properties.rs
+++ src/phy/opt/properties.rs
@@ -10,17 +10,31 @@ pub struct Properties {
     scat_coeff: f64,
     /// Absorption coefficient [m^-1].
     abs_coeff: f64,
+    /// Asymmetry parameter.
+    asym_param: f64,
+    /// Refractive index.
+    ref_index: f64,
 }
 
 impl Properties {
     /// Construct a new instance.
+    #[pre(0.0 < ref_index)]
     #[pre(0.0 < scat_coeff)]
     #[pre(0.0 < abs_coeff)]
-    pub fn new(scat_coeff: f64, abs_coeff: f64) -> Self {
+    #[pre(-1.0 < asym_param)]
+    #[pre(asym_param < 1.0)]
+    pub fn new(ref_index: f64, scat_coeff: f64, abs_coeff: f64, asym_param: f64) -> Self {
         Self {
+            ref_index,
             scat_coeff,
             abs_coeff,
+            asym_param,
         }
+    }
+
+    /// Get the refractive index.
+    pub fn ref_index(&self) -> f64 {
+        self.ref_index
     }
 
     /// Get the scattering coefficient.
@@ -31,5 +45,10 @@ impl Properties {
     /// Get the absorption coefficient.
     pub fn abs_coeff(&self) -> f64 {
         self.abs_coeff
+    }
+
+    /// Get the asymmetry parameter.
+    pub fn asym_param(&self) -> f64 {
+        self.asym_param
     }
 }
