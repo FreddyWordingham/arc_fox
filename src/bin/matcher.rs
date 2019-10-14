ceb918d47d52579e60e9d8986306d2620f9d2ec6
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 20:00:45 +0100

    Added serialisation support for kinetic physical properties.

--- src/bin/matcher.rs
+++ src/bin/matcher.rs
@@ -13,36 +13,51 @@ use std::collections::HashMap;
 fn main() {
     println!("Hello world!");
 
-    let mut materials = HashMap::new();
-    materials.insert(
+    let mut _materials = load_materials(vec![
         "stratum_corneum",
+        "living_epidermis",
+        "papillary_dermis",
+        "upper_blood_net_dermis",
+        "reticular_dermis",
+        "deep_blood_net_dermis",
+        "subcutaneous_fat",
+    ]);
+}
+
+fn load_materials(_mat_list: Vec<&str>) -> HashMap<String, Material> {
+    let mut materials = HashMap::new();
+
+    materials.insert(
+        "stratum_corneum".to_string(),
         Material::new(
             Some(OptProp::new(1.5, 100.0 * 1e3, 0.630 * 1e3, 0.86)),
             Some(KinProp::new(1.0)),
         ),
     );
     materials.insert(
-        "living_epidermis",
+        "living_epidermis".to_string(),
         Material::new(Some(OptProp::new(1.34, 45.0 * 1e3, 0.872 * 1e3, 0.8)), None),
     );
     materials.insert(
-        "papillary_dermis",
+        "papillary_dermis".to_string(),
         Material::new(Some(OptProp::new(1.4, 30.0 * 1e3, 0.106 * 1e3, 0.8)), None),
     );
     materials.insert(
-        "upper_blood_net_dermis",
+        "upper_blood_net_dermis".to_string(),
         Material::new(Some(OptProp::new(1.39, 35.0 * 1e3, 0.142 * 1e3, 0.8)), None),
     );
     materials.insert(
-        "reticular_dermis",
+        "reticular_dermis".to_string(),
         Material::new(Some(OptProp::new(1.4, 25.0 * 1e3, 0.106 * 1e3, 0.8)), None),
     );
     materials.insert(
-        "deep_blood_net_dermis",
+        "deep_blood_net_dermis".to_string(),
         Material::new(Some(OptProp::new(1.38, 30.0 * 1e3, 0.164 * 1e3, 0.8)), None),
     );
     materials.insert(
-        "subcutaneous_fat",
+        "subcutaneous_fat".to_string(),
         Material::new(Some(OptProp::new(1.44, 5.0 * 1e3, 0.104 * 1e3, 0.8)), None),
     );
+
+    materials
 }
--- src/phy/kin/properties.rs
+++ src/phy/kin/properties.rs
@@ -1,10 +1,13 @@
 //! Kinetic statistical properties structure.
 
+use crate::file::{as_json, from_json, Loadable, Saveable};
 use contracts::pre;
+use serde::{Deserialize, Serialize};
+use std::path::Path;
 
 /// Kinetic statistical physical properties.
 /// Contains parameters governing how number density changes over space with time.
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Properties {
     /// Diffusion coefficient [m^2/t].
     diff_coeff: f64,
@@ -20,5 +23,17 @@ impl Properties {
     /// Get the diffusion coefficient.
     pub fn diff_coeff(&self) -> f64 {
         self.diff_coeff
+    }
+}
+
+impl Saveable for Properties {
+    fn save(&self, path: &Path) {
+        as_json(self, path);
+    }
+}
+
+impl Loadable for Properties {
+    fn load(path: &Path) -> Self {
+        from_json(path)
     }
 }
