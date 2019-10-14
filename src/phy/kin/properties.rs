07bacf29b41ffb4fc035f7548ed09b38d7b0bb4a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 19:02:46 +0100

    Fleshed out kinetic properties struct.

--- src/phy/kin/properties.rs
+++ src/phy/kin/properties.rs
@@ -1,9 +1,24 @@
 //! Kinetic statistical properties structure.
 
+use contracts::pre;
+
 /// Kinetic statistical physical properties.
 /// Contains parameters governing how number density changes over space with time.
 #[derive(Debug)]
 pub struct Properties {
     /// Diffusion coefficient [m^2/t].
     diff_coeff: f64,
+}
+
+impl Properties {
+    /// Construct a new instance.
+    #[pre(0.0 < diff_coeff)]
+    pub fn new(diff_coeff: f64) -> Self {
+        Self { diff_coeff }
+    }
+
+    /// Get the diffusion coefficient.
+    pub fn diff_coeff(&self) -> f64 {
+        self.diff_coeff
+    }
 }
