9b8cbabc0fe6bc44f6710e67a0b3b3fb3d8535e0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 20:28:59 +0100

    Added material list loading.

--- src/bin/matcher.rs
+++ src/bin/matcher.rs
@@ -7,11 +7,7 @@
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
 
-use arc::{
-    dir::resources,
-    file::Saveable,
-    phy::{kin::Properties as KinProp, opt::Properties as OptProp, Material},
-};
+use arc::{dir::resources, file::Loadable, phy::Material};
 use std::{collections::HashMap, path::Path};
 
 fn main() {
@@ -19,7 +15,7 @@ fn main() {
 
     let res_dir = resources();
 
-    let materials = load_materials(
+    let _materials = load_materials(
         &res_dir.join("mats/"),
         vec![
             "stratum_corneum",
@@ -31,50 +27,18 @@ fn main() {
             "subcutaneous_fat",
         ],
     );
-
-    for (name, mat) in materials {
-        println!("Saving material: {}", name);
-        mat.save(&res_dir.join("mats/").join(format!("{}.json", name)));
-    }
 }
 
-fn load_materials(mat_dir: &Path, _mat_list: Vec<&str>) -> HashMap<String, Material> {
+fn load_materials(mat_dir: &Path, mat_list: Vec<&str>) -> HashMap<String, Material> {
     println!("Loading materials from: {}", mat_dir.display());
 
     let mut materials = HashMap::new();
+    for name in mat_list {
+        let path = mat_dir.join(format!("{}.json", name));
+        println!("Loading {}", path.display());
 
-    materials.insert(
-        "stratum_corneum".to_string(),
-        Material::new(
-            Some(OptProp::new(1.5, 100.0 * 1e3, 0.630 * 1e3, 0.86)),
-            Some(KinProp::new(1.0)),
-        ),
-    );
-    materials.insert(
-        "living_epidermis".to_string(),
-        Material::new(Some(OptProp::new(1.34, 45.0 * 1e3, 0.872 * 1e3, 0.8)), None),
-    );
-    materials.insert(
-        "papillary_dermis".to_string(),
-        Material::new(Some(OptProp::new(1.4, 30.0 * 1e3, 0.106 * 1e3, 0.8)), None),
-    );
-    materials.insert(
-        "upper_blood_net_dermis".to_string(),
-        Material::new(Some(OptProp::new(1.39, 35.0 * 1e3, 0.142 * 1e3, 0.8)), None),
-    );
-    materials.insert(
-        "reticular_dermis".to_string(),
-        Material::new(Some(OptProp::new(1.4, 25.0 * 1e3, 0.106 * 1e3, 0.8)), None),
-    );
-    materials.insert(
-        "deep_blood_net_dermis".to_string(),
-        Material::new(Some(OptProp::new(1.38, 30.0 * 1e3, 0.164 * 1e3, 0.8)), None),
-    );
-    materials.insert(
-        "subcutaneous_fat".to_string(),
-        Material::new(Some(OptProp::new(1.44, 5.0 * 1e3, 0.104 * 1e3, 0.8)), None),
-    );
-
+        materials.insert(name.to_string(), Material::load(&path));
+    }
     println!("Materials loaded!");
 
     materials
