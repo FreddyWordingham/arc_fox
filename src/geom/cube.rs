f0ac6570459f9d678aa0bc02c3c43e4c97bf7436
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 21:01:00 +0100

    Added surface implementatio to cube geometry.

--- src/geom/cube.rs
+++ src/geom/cube.rs
@@ -1,7 +1,9 @@
 //! Geometric cube structure.
 
+use super::{Ray, Surface};
+use crate::util::Container;
 use contracts::pre;
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Unit, Vector3};
 
 /// Axis-aligned box.
 /// Commonly used to partition the domain.
@@ -54,6 +56,102 @@ impl Cube {
             && (point.y <= self.maxs.y)
             && (self.mins.z <= point.z)
             && (point.z <= self.maxs.z)
+    }
+}
+
+impl Surface for Cube {
+    fn intersect(&self, ray: &Ray) -> bool {
+        let dir_frac = Vector3::new(
+            1.0 / ray.direction().x,
+            1.0 / ray.direction().y,
+            1.0 / ray.direction().z,
+        );
+
+        let tx0 = (self.mins.x - ray.origin().x) * dir_frac.x;
+        let tx1 = (self.maxs.x - ray.origin().x) * dir_frac.x;
+        let ty0 = (self.mins.y - ray.origin().y) * dir_frac.y;
+        let ty1 = (self.maxs.y - ray.origin().y) * dir_frac.y;
+        let tz0 = (self.mins.z - ray.origin().z) * dir_frac.z;
+        let tz1 = (self.maxs.z - ray.origin().z) * dir_frac.z;
+
+        let t_min = (tx0.min(tx1)).max(ty0.min(ty1)).max(tz0.min(tz1));
+        let t_max = (tx0.max(tx1)).min(ty0.max(ty1)).min(tz0.max(tz1));
+
+        !((t_max < 0.0) || (t_min > t_max))
+    }
+
+    fn distance(&self, ray: &Ray) -> Option<f64> {
+        let dir_frac = Vector3::new(
+            1.0 / ray.direction().x,
+            1.0 / ray.direction().y,
+            1.0 / ray.direction().z,
+        );
+
+        let t1 = (self.mins.x - ray.origin().x) * dir_frac.x;
+        let t2 = (self.maxs.x - ray.origin().x) * dir_frac.x;
+        let t3 = (self.mins.y - ray.origin().y) * dir_frac.y;
+        let t4 = (self.maxs.y - ray.origin().y) * dir_frac.y;
+        let t5 = (self.mins.z - ray.origin().z) * dir_frac.z;
+        let t6 = (self.maxs.z - ray.origin().z) * dir_frac.z;
+
+        let t_min = (t1.min(t2)).max(t3.min(t4)).max(t5.min(t6));
+        let t_max = (t1.max(t2)).min(t3.max(t4)).min(t5.max(t6));
+
+        if (t_max < 0.0) || (t_min > t_max) {
+            return None;
+        }
+
+        if t_min > 0.0 {
+            return Some(t_min);
+        }
+
+        Some(t_max)
+    }
+
+    fn distance_normal(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>)> {
+        let dir_frac = Vector3::new(
+            1.0 / ray.direction().x,
+            1.0 / ray.direction().y,
+            1.0 / ray.direction().z,
+        );
+
+        let t1 = Container::new(
+            (self.mins.x - ray.origin().x) * dir_frac.x,
+            -Vector3::x_axis(),
+        );
+        let t2 = Container::new(
+            (self.maxs.x - ray.origin().x) * dir_frac.x,
+            Vector3::x_axis(),
+        );
+        let t3 = Container::new(
+            (self.mins.y - ray.origin().y) * dir_frac.y,
+            -Vector3::y_axis(),
+        );
+        let t4 = Container::new(
+            (self.maxs.y - ray.origin().y) * dir_frac.y,
+            Vector3::y_axis(),
+        );
+        let t5 = Container::new(
+            (self.mins.z - ray.origin().z) * dir_frac.z,
+            -Vector3::z_axis(),
+        );
+        let t6 = Container::new(
+            (self.maxs.z - ray.origin().z) * dir_frac.z,
+            Vector3::z_axis(),
+        );
+
+        let t_min = (&t1.min(&t2)).max(&t3.min(&t4)).max(&t5.min(&t6));
+        let t_max = (&t1.max(&t2)).min(&t3.max(&t4)).min(&t5.max(&t6));
+
+        if (t_max < 0.0) || (t_min > t_max) {
+            return None;
+        }
+
+        if t_min > 0.0 {
+            return Some(t_min.separate());
+        }
+
+        Some(t_max.separate())
     }
 }
 
--- src/geom/triangle.rs
+++ src/geom/triangle.rs
@@ -1,9 +1,13 @@
 //! Geometric triangle structure with interpolated phong normals.
 
+use super::{Ray, Surface};
 use nalgebra::{Point3, Unit, Vector3};
 
+/// Parallel ray catch value.
+const EPSILON: f64 = 1.0e-6;
+
 /// Triangle in three-dimensions.
-/// Commonly used to compose triangle-meshes which in turn form surfaces.
+/// Commonly used to compose surfaces.
 #[derive(Debug)]
 pub struct Triangle {
     /// Vertex positions.
@@ -31,5 +35,121 @@ impl Triangle {
     /// Calculate the area of the triangle.
     pub fn area(&self) -> f64 {
         ((self.verts[1] - self.verts[0]).cross(&(self.verts[2] - self.verts[0]))).magnitude() / 2.0
+    }
+}
+
+impl Surface for Triangle {
+    fn intersect(&self, ray: &Ray) -> bool {
+        let e01 = self.verts[1] - self.verts[0];
+        let e02 = self.verts[2] - self.verts[0];
+
+        let h = ray.direction().cross(&e02);
+        let a = e01.dot(&h);
+
+        if a.abs() < EPSILON {
+            return false;
+        }
+
+        let f = 1.0 / a;
+        let s = ray.origin() - self.verts[0];
+        let u = f * s.dot(&h);
+
+        if (u < 0.0) || (u > 1.0) {
+            return false;
+        }
+
+        let q = s.cross(&e01);
+        let v = f * ray.direction().dot(&q);
+
+        if (v < 0.0) || ((u + v) > 1.0) {
+            return false;
+        }
+
+        let dist = f * e02.dot(&q);
+
+        if dist < EPSILON {
+            return false;
+        }
+
+        true
+    }
+
+    fn distance(&self, ray: &Ray) -> Option<f64> {
+        let e01 = self.verts[1] - self.verts[0];
+        let e02 = self.verts[2] - self.verts[0];
+
+        let h = ray.direction().cross(&e02);
+        let a = e01.dot(&h);
+
+        if a.abs() < EPSILON {
+            return None;
+        }
+
+        let f = 1.0 / a;
+        let s = ray.origin() - self.verts[0];
+        let u = f * s.dot(&h);
+
+        if (u < 0.0) || (u > 1.0) {
+            return None;
+        }
+
+        let q = s.cross(&e01);
+        let v = f * ray.direction().dot(&q);
+
+        if (v < 0.0) || ((u + v) > 1.0) {
+            return None;
+        }
+
+        let dist = f * e02.dot(&q);
+
+        if dist < EPSILON {
+            return None;
+        }
+
+        Some(dist)
+    }
+
+    fn distance_normal(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>)> {
+        let e01 = self.verts()[1] - self.verts()[0];
+        let e02 = self.verts()[2] - self.verts()[0];
+
+        let h = ray.direction().cross(&e02);
+        let a = e01.dot(&h);
+
+        if a.abs() < EPSILON {
+            return None;
+        }
+
+        let f = 1.0 / a;
+        let s = ray.origin() - self.verts()[0];
+        let u = f * s.dot(&h);
+
+        if (u < 0.0) || (u > 1.0) {
+            return None;
+        }
+
+        let q = s.cross(&e01);
+        let v = f * ray.direction().dot(&q);
+
+        if (v < 0.0) || ((u + v) > 1.0) {
+            return None;
+        }
+
+        let dist = f * e02.dot(&q);
+
+        if dist < EPSILON {
+            return None;
+        }
+
+        let w = 1.0 - u - v;
+
+        Some((
+            dist,
+            Unit::new_normalize(
+                (self.norms[1].into_inner() * u)
+                    + (self.norms[2].into_inner() * v)
+                    + (self.norms[0].into_inner() * w),
+            ),
+        ))
     }
 }
