089e5f1630925886260631e3d0ceb8e5696849d1
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-14 19:48:16 +0100

    Added materials hashmap.

--- src/bin/matcher.rs
+++ src/bin/matcher.rs
@@ -8,24 +8,41 @@
 #![warn(clippy::missing_docs_in_private_items)]
 
 use arc::phy::{kin::Properties as KinProp, opt::Properties as OptProp, Material};
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello world!");
 
-    let _stratum_corneum = Material::new(
-        Some(OptProp::new(1.5, 100.0 * 1e3, 0.630 * 1e3, 0.86)),
-        Some(KinProp::new(1.0)),
+    let mut materials = HashMap::new();
+    materials.insert(
+        "stratum_corneum",
+        Material::new(
+            Some(OptProp::new(1.5, 100.0 * 1e3, 0.630 * 1e3, 0.86)),
+            Some(KinProp::new(1.0)),
+        ),
     );
-    let _living_epidermis =
-        Material::new(Some(OptProp::new(1.34, 45.0 * 1e3, 0.872 * 1e3, 0.8)), None);
-    let _papillary_dermis =
-        Material::new(Some(OptProp::new(1.4, 30.0 * 1e3, 0.106 * 1e3, 0.8)), None);
-    let _upper_blood_net_dermis =
-        Material::new(Some(OptProp::new(1.39, 35.0 * 1e3, 0.142 * 1e3, 0.8)), None);
-    let _reticular_dermis =
-        Material::new(Some(OptProp::new(1.4, 25.0 * 1e3, 0.106 * 1e3, 0.8)), None);
-    let _deep_blood_net_dermis =
-        Material::new(Some(OptProp::new(1.38, 30.0 * 1e3, 0.164 * 1e3, 0.8)), None);
-    let _subcutaneous_fat =
-        Material::new(Some(OptProp::new(1.44, 5.0 * 1e3, 0.104 * 1e3, 0.8)), None);
+    materials.insert(
+        "living_epidermis",
+        Material::new(Some(OptProp::new(1.34, 45.0 * 1e3, 0.872 * 1e3, 0.8)), None),
+    );
+    materials.insert(
+        "papillary_dermis",
+        Material::new(Some(OptProp::new(1.4, 30.0 * 1e3, 0.106 * 1e3, 0.8)), None),
+    );
+    materials.insert(
+        "upper_blood_net_dermis",
+        Material::new(Some(OptProp::new(1.39, 35.0 * 1e3, 0.142 * 1e3, 0.8)), None),
+    );
+    materials.insert(
+        "reticular_dermis",
+        Material::new(Some(OptProp::new(1.4, 25.0 * 1e3, 0.106 * 1e3, 0.8)), None),
+    );
+    materials.insert(
+        "deep_blood_net_dermis",
+        Material::new(Some(OptProp::new(1.38, 30.0 * 1e3, 0.164 * 1e3, 0.8)), None),
+    );
+    materials.insert(
+        "subcutaneous_fat",
+        Material::new(Some(OptProp::new(1.44, 5.0 * 1e3, 0.104 * 1e3, 0.8)), None),
+    );
 }
