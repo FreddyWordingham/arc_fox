2d02ab58be241775787433e6b60b7988aa670f96
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-18 14:32:37 +0100

    Added boundary field to world boundary struct.

--- src/world/boundary.rs
+++ src/world/boundary.rs
@@ -1,12 +1,15 @@
 //! Material boundary structure.
 
-use crate::{geom::Triangle, phy::Material};
+use crate::{geom::{Triangle, Cube}, phy::Material};
+use contracts::pre;
 
 /// Material boundary structure forming the boundary between two materials.
 #[derive(Debug)]
 pub struct Boundary<'a> {
     /// Bounding triangles.
     tris: Vec<Triangle>,
+    /// Bounding box.
+    boundary: Cube,
     /// Inside material.
     in_mat: &'a Material,
     /// Outside material.
@@ -15,9 +18,28 @@ pub struct Boundary<'a> {
 
 impl<'a> Boundary<'a> {
     /// Construct a new instance.
+    #[pre(!tris.is_empty())]
     pub fn new(tris: Vec<Triangle>, in_mat: &'a Material, out_mat: &'a Material) -> Self {
+        let mut mins = tris[0].verts()[0];
+        let mut maxs = mins;
+
+        for tri in tris.iter() {
+            for vert in tri.verts() {
+                for d in 0..3 {
+                    if vert[d] < mins[d] {
+                        mins[d] = vert[d];
+                    }
+
+                    if vert[d] > maxs[d] {
+                        maxs[d] = vert[d];
+                    }
+                }
+            }
+        }
+
         Self {
             tris,
+            boundary: Cube::new(mins, maxs),
             in_mat,
             out_mat,
         }
