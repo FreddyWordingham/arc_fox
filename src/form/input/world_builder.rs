e86ad28674648fd5cca7744bbd2c064779b573e5
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-21 19:08:21 +0100

    Added proto-directory field to WorldBuilder input form struct.

--- src/form/input/world_builder.rs
+++ src/form/input/world_builder.rs
@@ -1,5 +1,6 @@
 //! World builder input form.
 
+use super::super::proto;
 use crate::file::{as_json, from_json, Loadable, Saveable};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
@@ -7,22 +8,19 @@ use std::path::Path;
 /// Input form structure containing all information required to run the world_builder binary.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct WorldBuilder {
-    /// Minimum bound.
-    mins: [f64; 3],
-    /// Maximum bound.
-    maxs: [f64; 3],
-    /// Number of cells.
-    num_cells: [usize; 3],
+    /// Directory information.
+    dir: proto::Dir,
 }
 
 impl WorldBuilder {
     /// Create an example world.
-    pub fn example() -> Self {
-        Self {
-            mins: [-1.0, -1.0, -1.0],
-            maxs: [1.0, 1.0, 1.0],
-            num_cells: [8, 8, 8],
-        }
+    pub fn example(dir: proto::Dir) -> Self {
+        Self { dir }
+    }
+
+    /// Reference the directory proto-structure.
+    pub fn dir(&self) -> &proto::Dir {
+        &self.dir
     }
 }
 
