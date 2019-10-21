802d4887994983106bf0ee0b805c582b2f808c62
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-21 20:28:49 +0100

    Renamed surface to boundary.

--- src/world/domain.rs
+++ src/world/domain.rs
@@ -8,8 +8,8 @@ use std::fmt::{Display, Formatter, Result};
 /// Domain with regular split grid partitioning.
 #[derive(Debug)]
 pub struct Domain {
-    /// Surface.
-    surface: Cube,
+    /// Boundary.
+    boundary: Cube,
     /// Number of cells in each axis.
     num_cells: [usize; 3],
     /// Cell size.
@@ -21,22 +21,22 @@ impl Domain {
     #[pre(num_cells[0] > 0)]
     #[pre(num_cells[1] > 0)]
     #[pre(num_cells[2] > 0)]
-    pub fn new(surface: Cube, num_cells: [usize; 3]) -> Self {
-        let mut cell_size = surface.widths();
+    pub fn new(boundary: Cube, num_cells: [usize; 3]) -> Self {
+        let mut cell_size = boundary.widths();
         cell_size.x /= num_cells[0] as f64;
         cell_size.y /= num_cells[1] as f64;
         cell_size.z /= num_cells[2] as f64;
 
         Self {
-            surface,
+            boundary,
             num_cells,
             cell_size,
         }
     }
 
-    /// Reference the surface geometry.
-    pub fn surface(&self) -> &Cube {
-        &self.surface
+    /// Reference the boundary geometry.
+    pub fn boundary(&self) -> &Cube {
+        &self.boundary
     }
 
     /// Reference the number of cells.
@@ -49,12 +49,12 @@ impl Domain {
         &self.cell_size
     }
 
-    /// Construct the surface of a cell at a given index.
+    /// Construct the boundary of a cell at a given index.
     #[pre(index[0] < self.num_cells[0])]
     #[pre(index[1] < self.num_cells[1])]
     #[pre(index[2] < self.num_cells[2])]
-    pub fn cell_surface(&self, index: [usize; 3]) -> Cube {
-        let mut min = self.surface.mins().clone();
+    pub fn cell_boundary(&self, index: [usize; 3]) -> Cube {
+        let mut min = self.boundary.mins().clone();
         min.x += self.cell_size.x * index[0] as f64;
         min.y += self.cell_size.y * index[1] as f64;
         min.z += self.cell_size.z * index[2] as f64;
@@ -65,8 +65,8 @@ impl Domain {
 
 impl Display for Domain {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        let mins = self.surface.mins();
-        let maxs = self.surface.maxs();
+        let mins = self.boundary.mins();
+        let maxs = self.boundary.maxs();
 
         writeln!(f, "Mins     : {},\t{},\t{}", mins.x, mins.y, mins.z)?;
         writeln!(f, "Maxs     : {},\t{},\t{}", maxs.x, maxs.y, maxs.z)?;
