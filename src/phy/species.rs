9e3347611758a7f54dc2f0c98a7076b0881ec142
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-23 12:49:22 +0100

    Added physical species structure.

--- src/phy/species.rs
+++ src/phy/species.rs
@@ -1,7 +1,9 @@
 //! Chemical species information structure.
 
+use crate::file::{as_json, from_json, Loadable, Saveable};
 use contracts::pre;
 use serde::{Deserialize, Serialize};
+use std::path::Path;
 
 /// Chemical species information.
 #[derive(Debug, Deserialize, Serialize)]
@@ -15,5 +17,17 @@ impl Species {
     #[pre(radius > 0.0)]
     pub fn new(radius: f64) -> Self {
         Self { radius }
+    }
+}
+
+impl Saveable for Species {
+    fn save(&self, path: &Path) {
+        as_json(self, path);
+    }
+}
+
+impl Loadable for Species {
+    fn load(path: &Path) -> Self {
+        from_json(path)
     }
 }
