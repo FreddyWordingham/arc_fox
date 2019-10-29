eb7ecc7f20dd32d49cbb65dbcd859bc692617903
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-29 22:51:36 +0000

    Added aabb geometry.

--- src/geom.rs
+++ src/geom.rs
@@ -1,7 +1,9 @@
 //! Geometric shapes and their relative properties in space.
 
+pub mod aabb;
 pub mod ray;
 pub mod traceable;
 
+pub use self::aabb::*;
 pub use self::ray::*;
 pub use self::traceable::*;
