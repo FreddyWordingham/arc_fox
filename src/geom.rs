d0f7b26fa54838c737d351125e928ef3e61e61b9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-29 22:28:34 +0000

    Added geometry module.

---
+++ src/geom.rs
@@ -0,0 +1,5 @@
+//! Geometric shapes and their relative properties in space.
+
+pub mod ray;
+
+pub use self::ray::*;
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,5 @@
 #![allow(unknown_lints)]
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
+
+pub mod geom;
