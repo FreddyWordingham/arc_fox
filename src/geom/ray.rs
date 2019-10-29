8f6b9195eba6de13d4d4ea54195cd7eae286d7e7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-29 22:36:38 +0000

    Simplified ray structure access methods.

--- src/geom/ray.rs
+++ src/geom/ray.rs
@@ -7,24 +7,14 @@ use nalgebra::{Point3, Unit, Vector3};
 #[derive(Debug)]
 pub struct Ray {
     /// Origin position.
-    pos: Point3<f64>,
+    pub pos: Point3<f64>,
     /// Facing direction.
-    dir: Unit<Vector3<f64>>,
+    pub dir: Unit<Vector3<f64>>,
 }
 
 impl Ray {
     /// Construct a new instance.
     pub fn new(pos: Point3<f64>, dir: Unit<Vector3<f64>>) -> Self {
         Self { pos, dir }
-    }
-
-    /// Reference the origin position.
-    pub fn pos(&mut self) -> &mut Point3<f64> {
-        &mut self.pos
-    }
-
-    /// Reference the facing direction.
-    pub fn dir(&mut self) -> &mut Unit<Vector3<f64>> {
-        &mut self.dir
     }
 }
