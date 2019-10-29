1e3ec1c15328ff1ac7e940fc5634fc08d2d4cc3e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-29 11:46:40 +0000

    Added startup to ptfe binary.

---
+++ src/bin/ptfe.rs
@@ -0,0 +1,24 @@
+//! PTFE investigation.
+
+use arc::{
+    file::Loadable,
+    // file::Saveable,
+    form::input::Ptfe as PtfeForm,
+    util::{get_args, title},
+};
+use log::info;
+use std::path::Path;
+
+fn main() {
+    // Title.
+    title("PTFE");
+    colog::init();
+
+    // Start up.
+    let args = get_args(vec!["<manifest.json>".to_string()]);
+    let input_file_path = Path::new(&args[1]);
+
+    // Load manifest.
+    info!("Loading input form: {}", input_file_path.display());
+    let form = PtfeForm::example();
+}
