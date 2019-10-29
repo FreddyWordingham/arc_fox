f315114ac6ce52d2ea4cffd5f1e8502cd12eea8f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-29 22:38:41 +0000

    Added traceable geometry trait.

--- src/geom.rs
+++ src/geom.rs
@@ -1,5 +1,7 @@
 //! Geometric shapes and their relative properties in space.
 
 pub mod ray;
+pub mod traceable;
 
 pub use self::ray::*;
+pub use self::traceable::*;
---
+++ src/geom/traceable.rs
@@ -0,0 +1,16 @@
+//! Surface collidable trait.
+
+use super::Ray;
+use nalgebra::{Unit, Vector3};
+
+/// Types implementing this trait can be traced using a ray.
+pub trait Traceable {
+    /// Determine if an interaction occurs at all.
+    fn intersect(&self, ray: &Ray) -> bool;
+
+    /// Distance to the surface along the ray's line of travel.
+    fn distance(&self, ray: &Ray) -> Option<f64>;
+
+    /// Distance to the surface along the ray's line of travel and normal unit vector at the point of collision.
+    fn distance_normal(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>)>;
+}
