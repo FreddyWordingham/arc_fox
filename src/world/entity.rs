3704855ea42609d06a02d2964722925b8d6fc7a7
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-31 14:16:45 +0000

    Added materials to entities.

--- src/world/entity.rs
+++ src/world/entity.rs
@@ -1,24 +1,42 @@
 //! World entity structure.
 
-use crate::geom::Shape;
+use crate::{geom::Shape, phys::Material};
 use contracts::pre;
 
 /// World entity structure.
 /// Binds a material to a geometry.
-pub struct Entity {
+pub struct Entity<'a> {
     /// Surface geometry.
     surfs: Vec<Box<dyn Shape>>,
+    /// Inside material.
+    in_mat: &'a Material,
+    /// Outside material.
+    out_mat: &'a Material,
 }
 
-impl Entity {
+impl<'a> Entity<'a> {
     /// Construct a new instance.
     #[pre(!surfs.is_empty())]
-    pub fn new(surfs: Vec<Box<dyn Shape>>) -> Self {
-        Self { surfs }
+    pub fn new(surfs: Vec<Box<dyn Shape>>, in_mat: &'a Material, out_mat: &'a Material) -> Self {
+        Self {
+            surfs,
+            in_mat,
+            out_mat,
+        }
     }
 
     /// Access the surface list.
     pub fn surfs(&self) -> &Vec<Box<dyn Shape>> {
         &self.surfs
+    }
+
+    /// Reference the inside material.
+    pub fn in_mat(&self) -> &'a Material {
+        &self.in_mat
+    }
+
+    /// Reference the outside material.
+    pub fn out_mat(&self) -> &'a Material {
+        &self.out_mat
     }
 }
