c183cd84c9ebebc2e27e899ae67743764f4ec137
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-10-31 13:33:18 +0000

    Added optical environment generator method to physical materials.

--- src/phys/material.rs
+++ src/phys/material.rs
@@ -1,5 +1,6 @@
 //! Physical material structure.
 
+use super::opt::Environment;
 use crate::{math::Formula, util::Range};
 use contracts::pre;
 use serde::{Deserialize, Serialize};
@@ -40,5 +41,17 @@ impl Material {
             shift_coeff,
             asym,
         }
+    }
+
+    /// Get the optical environment for a given wavelength.
+    #[pre(self.range.contains(w))]
+    pub fn env(&self, w: f64) -> Environment {
+        Environment::new(
+            self.ref_index.calc(w),
+            self.scat_coeff.calc(w),
+            self.abs_coeff.calc(w),
+            self.shift_coeff.calc(w),
+            self.asym.calc(w),
+        )
     }
 }
