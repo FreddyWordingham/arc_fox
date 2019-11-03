007cfa37e80931e8c1118a051c8d16b0990fe27a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-03 17:18:15 +0000

    Added library core file.

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Arc library core.
+
+#![doc(html_root_url = "https://freddywordingham.github.io/arc/")]
+#![allow(dead_code)]
+#![allow(clippy::all)]
+#![allow(unknown_lints)]
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
