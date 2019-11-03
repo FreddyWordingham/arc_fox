ef0b073c2fa033f1c7bfee0e9c81064928af3fa0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-03 17:30:49 +0000

    Added title print.

---
+++ src/bin/cartographer.rs
@@ -0,0 +1,14 @@
+//! Cartographer binary.
+//! Creates a data cube mapping materials within a volume.
+
+use arc::print;
+
+fn main() {
+    title();
+}
+
+fn title() {
+    print::title("PTFE");
+
+    colog::init();
+}
