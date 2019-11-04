40f142b04159660b5452b31bd2072c6ea07e75f2
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-04 01:18:55 +0000

    Added cell contents.

--- src/dom/cell.rs
+++ src/dom/cell.rs
@@ -6,6 +6,8 @@ use crate::{
     phys::Material,
     world::{mat_at_point_from_list, mat_at_point_from_map, EntMap, Entity},
 };
+use contracts::pre;
+use nalgebra::Point3;
 
 /// Single domain cell.
 pub struct Cell<'a> {
@@ -53,5 +55,25 @@ impl<'a> Cell<'a> {
     /// Reference the boundary.
     pub fn aabb(&self) -> &Aabb {
         &self.aabb
+    }
+
+    /// Reference the intersecting entity list.
+    pub fn ent_list(&self) -> &Vec<(&'a Entity<'a>, Vec<&'a Shape>)> {
+        &self.ent_list
+    }
+
+    /// Reference the central material.
+    pub fn centre_mat(&self) -> &Material {
+        &self.centre_mat
+    }
+
+    /// Reference the material at the given point.
+    #[pre(self.aabb.contains(p))]
+    pub fn mat_at_point(&self, p: &Point3<f64>) -> &Material {
+        if self.ent_list.is_empty() {
+            return self.centre_mat;
+        }
+
+        mat_at_point_from_list(&p, &self.aabb, &self.ent_list)
     }
 }
