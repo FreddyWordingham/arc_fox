e82b827d0b6412f556f2d7293e3bc3bfe9de97ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-04 13:45:29 +0000

    Implemented emitter trait for point3.

--- src/rt/emitter.rs
+++ src/rt/emitter.rs
@@ -1,10 +1,28 @@
 //! Ray emitter trait.
 
-use rand::rngs::ThreadRng;
+use rand::{rngs::ThreadRng, Rng};
 use super::Ray;
+use nalgebra::{Point3, Unit, Vector3};
+use std::f64::consts::PI;
 
 /// Types implementing this trait can emit rays.
 pub trait Emitter {
     /// Emit a new ray.
-    fn emit(&self, rng: &mut ThreadRng) -> &Ray;
+    fn emit(&self, rng: &mut ThreadRng) -> Ray;
+}
+
+impl Emitter for Point3<f64> {
+    fn emit(&self, rng: &mut ThreadRng) -> Ray {
+        let theta = rng.gen_range(0.0, 2.0 * PI);
+        let z = rng.gen_range(-1.0f64, 1.0);
+
+        Ray::new(
+            *self,
+            Unit::new_normalize(Vector3::new(
+                (1.0 - z.powi(2)).sqrt() * theta.cos(),
+                (1.0 - z.powi(2)).sqrt() * theta.sin(),
+                z,
+            )),
+        )
+    }
 }
