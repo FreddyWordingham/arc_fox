70b72a02cb685e914499defae3f3af3d94b43597
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-06 17:09:52 +0000

    Added parameters to record.

--- src/data/record.rs
+++ src/data/record.rs
@@ -8,12 +8,20 @@ use std::ops::{Add, AddAssign};
 pub struct Record {
     /// Total weight of photon emissions.
     emissions: f64,
+    /// Total weight of scattering events.
+    scatters: f64,
+    /// Total weight of absorption events.
+    absorptions: f64,
 }
 
 impl Record {
     /// Construct a new instance.
     pub fn new() -> Self {
-        Self { emissions: 0.0 }
+        Self {
+            emissions: 0.0,
+            scatters: 0.0,
+            absorptions: 0.0,
+        }
     }
 
     /// Get the number of recorded emissions.
@@ -21,10 +29,32 @@ impl Record {
         self.emissions
     }
 
+    /// Get the number of recorded scatterings.
+    pub fn scatters(&self) -> f64 {
+        self.scatters
+    }
+
+    /// Get the number of recorded absorptions.
+    pub fn absorptions(&self) -> f64 {
+        self.absorptions
+    }
+
+    /// Increase the number of recorded scatterings.
+    #[pre(w > 0.0)]
+    pub fn increase_scatters(&mut self, w: f64) {
+        self.scatters += w;
+    }
+
     /// Increase the number of recorded emissions.
     #[pre(w > 0.0)]
     pub fn increase_emissions(&mut self, w: f64) {
         self.emissions += w;
+    }
+
+    /// Increase the number of recorded absorptions.
+    #[pre(w > 0.0)]
+    pub fn increase_absorptions(&mut self, w: f64) {
+        self.absorptions += w;
     }
 }
 
@@ -34,6 +64,8 @@ impl Add<&Self> for Record {
     fn add(self, rhs: &Self) -> Self {
         Self {
             emissions: self.emissions + rhs.emissions,
+            scatters: self.scatters + rhs.scatters,
+            absorptions: self.absorptions + rhs.absorptions,
         }
     }
 }
@@ -41,11 +73,15 @@ impl Add<&Self> for Record {
 impl AddAssign for Record {
     fn add_assign(&mut self, rhs: Self) {
         self.emissions += rhs.emissions;
+        self.scatters += rhs.scatters;
+        self.absorptions += rhs.absorptions;
     }
 }
 
 impl AddAssign<&Self> for Record {
     fn add_assign(&mut self, rhs: &Self) {
         self.emissions += rhs.emissions;
+        self.scatters += rhs.scatters;
+        self.absorptions += rhs.absorptions;
     }
 }
