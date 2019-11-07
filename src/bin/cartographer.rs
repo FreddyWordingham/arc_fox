bc2a4d74ca06734c77965ddb4bbf680050c52e19
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-07 14:10:42 +0000

    Added distance travelled tracking.

--- src/bin/cartographer.rs
+++ src/bin/cartographer.rs
@@ -68,23 +68,27 @@ fn main() {
     let mut emissions = Vec::with_capacity(uni.grid().layout().total_indices());
     let mut scatters = Vec::with_capacity(uni.grid().layout().total_indices());
     let mut absorptions = Vec::with_capacity(uni.grid().layout().total_indices());
+    let mut dist_travelled = Vec::with_capacity(uni.grid().layout().total_indices());
     for rec in recs.iter() {
         emissions.push(rec.emissions());
         scatters.push(rec.scatters());
         absorptions.push(rec.absorptions());
+        dist_travelled.push(rec.absorptions());
     }
     let emissions = Array3::from_shape_vec(uni.grid().layout().nis, emissions).unwrap();
     let scatters = Array3::from_shape_vec(uni.grid().layout().nis, scatters).unwrap();
     let absorptions = Array3::from_shape_vec(uni.grid().layout().nis, absorptions).unwrap();
+    let dist_travelled = Array3::from_shape_vec(uni.grid().layout().nis, dist_travelled).unwrap();
 
     print::section("Output");
-    info!("Saving emissions data.");
+    info!("Saving output data.");
     save_as_netcdf(
-        &output.join("emissions.nc"),
+        &output.join("output.nc"),
         vec![
             ("emissions", &emissions),
             ("scatters", &scatters),
             ("absorptions", &absorptions),
+            ("dist_travelled", &dist_travelled),
         ],
     );
 
