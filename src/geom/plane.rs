c1dd5e442c4d6f2f92129c98db380abc941e8e37
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-08 12:40:43 +0000

    Implemented traceable trait for plane struct.

--- src/geom/plane.rs
+++ src/geom/plane.rs
@@ -1,5 +1,7 @@
 //! Plane structure.
 
+use super::EPSILON;
+use crate::rt::{Ray, Traceable};
 use nalgebra::{Point3, Unit, Vector3};
 
 /// Plane geometry.
@@ -14,5 +16,36 @@ impl Plane {
     /// Construct a new instance.
     pub fn new(pos: Point3<f64>, norm: Unit<Vector3<f64>>) -> Self {
         Self { pos, norm }
+    }
+}
+
+impl Traceable for Plane {
+    fn hit(&self, ray: &Ray) -> bool {
+        self.dist(ray).is_some()
+    }
+
+    fn dist(&self, ray: &Ray) -> Option<f64> {
+        let d = self.norm.dot(&ray.dir);
+
+        if d.abs() > EPSILON {
+            let rp = self.pos - ray.pos;
+            let dist = rp.dot(&self.norm) / d;
+
+            if dist < 0.0 {
+                return None;
+            }
+
+            return Some(dist);
+        }
+
+        None
+    }
+
+    fn dist_norm(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>)> {
+        if let Some(dist) = self.dist(ray) {
+            return Some((dist, self.norm));
+        }
+
+        None
     }
 }
