756b0970b16eaecf492422e1118cb7076449b34e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-08 20:01:14 +0000

    Added touchable method to triangle struct.

--- src/geom.rs
+++ src/geom.rs
@@ -5,12 +5,14 @@ pub mod collision;
 pub mod container;
 pub mod plane;
 pub mod touchable;
+pub mod triangle;
 
 pub use self::aabb::*;
 pub use self::collision::*;
 pub use self::container::*;
 pub use self::plane::*;
 pub use self::touchable::*;
+pub use self::triangle::*;
 
 /// Parallel ray catch value.
 const EPSILON: f64 = 1.0e-6;
