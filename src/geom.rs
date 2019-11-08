1aa7b7578dfc66fac6887af775f44fd6b3067667
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-08 18:50:09 +0000

    Added aabb struct and collision and container traits.

--- src/geom.rs
+++ src/geom.rs
@@ -1,4 +1,12 @@
 //! Geometric shape enumeration.
 
+pub mod aabb;
+pub mod collision;
+pub mod container;
+
+pub use self::aabb::*;
+pub use self::collision::*;
+pub use self::container::*;
+
 /// Parallel ray catch value.
 const EPSILON: f64 = 1.0e-6;
