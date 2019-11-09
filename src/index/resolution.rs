626885ec3d5d2f308dfcd0aa8902bdc3eb4d14d9
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-09 20:14:49 +0000

    Added index functions and structs.

---
+++ src/index/resolution.rs
@@ -0,0 +1,44 @@
+//! Layout structure.
+
+use contracts::pre;
+
+/// Three-dimensional resolution structure.
+/// Used by domain grids.
+pub struct Resolution {
+    /// Number of indices in each dimension.
+    pub nis: [usize; 3],
+}
+
+impl Resolution {
+    #[pre(xi > 0)]
+    #[pre(yi > 0)]
+    #[pre(zi > 0)]
+    pub fn new(xi: usize, yi: usize, zi: usize) -> Self {
+        Self { nis: [xi, yi, zi] }
+    }
+
+    /// Get the total number of indices.
+    pub fn total_indices(&self) -> usize {
+        self.nis[0] * self.nis[1] * self.nis[2]
+    }
+
+    /// Get the number of splits along the x-axis.
+    pub fn x(&self) -> usize {
+        self.nis[0]
+    }
+
+    /// Get the number of splits along the y-axis.
+    pub fn y(&self) -> usize {
+        self.nis[1]
+    }
+
+    /// Get the number of splits along the z-axis.
+    pub fn z(&self) -> usize {
+        self.nis[2]
+    }
+
+    /// Determine if the given index is contained within the layout.
+    pub fn contains(&self, index: &[usize; 3]) -> bool {
+        (index[0] < self.nis[0]) && (index[1] < self.nis[1]) && (index[2] < self.nis[2])
+    }
+}
