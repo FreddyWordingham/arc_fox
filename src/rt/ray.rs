d48bc3b2ce27f69228cda320910807b46340d5eb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-09 20:40:20 +0000

    Added ray structure.

---
+++ src/rt/ray.rs
@@ -0,0 +1,40 @@
+//! Geometric ray structure.
+
+use contracts::pre;
+use nalgebra::{Point3, Rotation3, Unit, Vector3};
+use std::f64::consts::{FRAC_PI_2, PI};
+
+/// Line with an origin point that extends infinitely in one direction.
+/// Used to determine the distance to a geometric shape.
+pub struct Ray {
+    /// Origin position.
+    pub pos: Point3<f64>,
+    /// Facing direction.
+    pub dir: Unit<Vector3<f64>>,
+}
+
+impl Ray {
+    /// Construct a new instance.
+    #[pre(dir.magnitude() == 1.0)]
+    pub fn new(pos: Point3<f64>, dir: Unit<Vector3<f64>>) -> Self {
+        Self { pos, dir }
+    }
+
+    /// Move along the direction the given distance.
+    #[pre(dist > 0.0)]
+    pub fn travel(&mut self, dist: f64) {
+        self.pos += self.dir.as_ref() * dist;
+    }
+
+    /// Pitch towards the z-axis and then roll around previous direction.
+    #[pre(self.dir.z.abs() != 1.0)]
+    #[post(self.dir.mag() == 1.0)]
+    pub fn rotate(&mut self, pitch: f64, roll: f64) {
+        let pitch_axis = Unit::new_normalize(self.dir.cross(&Vector3::z_axis()));
+        let pitch_rot = Rotation3::from_axis_angle(&pitch_axis, pitch);
+
+        let roll_rot = Rotation3::from_axis_angle(&self.dir, roll);
+
+        self.dir = roll_rot * pitch_rot * self.dir;
+    }
+}
