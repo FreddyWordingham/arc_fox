5d673072a5bf4fc200832dc88b6fabf86215b2c0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-10 23:40:00 +0000

    Added world entity struct.

---
+++ src/world/entity.rs
@@ -0,0 +1,41 @@
+//! World entity structure.
+
+use super::Material;
+use crate::geom::Mesh;
+
+/// World entity structure.
+/// Binds a material to a shape.
+pub struct Entity<'a> {
+    /// Surface mesh.
+    mesh: Mesh,
+    /// Inside material.
+    in_mat: &'a Material,
+    /// Outside material.
+    out_mat: &'a Material,
+}
+
+impl<'a> Entity<'a> {
+    /// Construct a new instance.
+    pub fn new(mesh: Mesh, in_mat: &'a Material, out_mat: &'a Material) -> Self {
+        Self {
+            mesh,
+            in_mat,
+            out_mat,
+        }
+    }
+
+    /// Reference the surface mesh.
+    pub fn mesh(&self) -> &Mesh {
+        &self.mesh
+    }
+
+    /// Reference the inside material.
+    pub fn in_mat(&self) -> &Material {
+        &self.in_mat
+    }
+
+    /// Reference the outside material.
+    pub fn out_mat(&self) -> &Material {
+        &self.out_mat
+    }
+}
