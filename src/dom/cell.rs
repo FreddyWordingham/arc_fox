5b1bffd8bf327abf4cb6152461f020c361d4882d
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-11 00:47:26 +0000

    Added cell constructor method.

--- src/dom/cell.rs
+++ src/dom/cell.rs
@@ -1,11 +1,44 @@
 //! Cell structure.
 
-use crate::{data::Record, geom::Aabb};
+use crate::{
+    data::Record,
+    geom::{Aabb, Collision, Triangle},
+    world::Entity,
+};
 
 /// Single domain cell.
-pub struct Cell {
-    /// Record.
-    rec: Record,
+pub struct Cell<'a> {
     /// Boundary.
     aabb: Aabb,
+    /// Record.
+    rec: Record,
+    /// Intersecting entity triangles.
+    ent_tris: Vec<(&'a Entity<'a>, Vec<&'a Triangle>)>,
+}
+
+impl<'a> Cell<'a> {
+    /// Construct a new instance.
+    pub fn new(aabb: Aabb, ents: &'a Vec<Entity>) -> Self {
+        let mut ent_tris = Vec::new();
+        for ent in ents {
+            if ent.mesh().overlap(&aabb) {
+                let mut list = Vec::new();
+                for tri in ent.mesh().tris() {
+                    if tri.overlap(&aabb) {
+                        list.push(tri);
+                    }
+                }
+
+                if !list.is_empty() {
+                    ent_tris.push((ent, list));
+                }
+            }
+        }
+
+        Self {
+            aabb,
+            rec: Record::new(),
+            ent_tris,
+        }
+    }
 }
