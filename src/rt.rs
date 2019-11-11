86afd48d4eb1790e71dc4c7e2692210183df77c3
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-11 00:02:21 +0000

    Added spotlight emitter.

--- src/rt.rs
+++ src/rt.rs
@@ -1,9 +1,11 @@
 //! Ray tracing structures and traits.
 
+pub mod emitter;
 pub mod gate;
 pub mod ray;
 pub mod traceable;
 
+pub use self::emitter::*;
 pub use self::gate::*;
 pub use self::ray::*;
 pub use self::traceable::*;
