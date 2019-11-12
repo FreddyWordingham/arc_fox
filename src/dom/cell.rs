795fa61f63b83528e07fd721733eae41da13efeb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-12 02:04:52 +0000

    Added distance methods to cell.

--- src/dom/cell.rs
+++ src/dom/cell.rs
@@ -4,9 +4,11 @@ use super::SIGMA;
 use crate::{
     data::Record,
     geom::{Aabb, Collision, Triangle},
+    rt::{Ray, Traceable},
     world::{mat_at_pos_from_list, mat_at_pos_from_sublist, Entity, Material},
 };
 use contracts::pre;
+use nalgebra::{Point3, Unit, Vector3};
 
 /// Single domain cell.
 #[derive(Debug)]
@@ -56,14 +58,66 @@ impl<'a> Cell<'a> {
         }
     }
 
-    /// Check if the cell contains intersecting triangles.
-    pub fn is_empty(&self) -> bool {
-        self.ent_tris.is_empty()
+    /// Reference the boundary.
+    pub fn aabb(&self) -> &Aabb {
+        &self.aabb
+    }
+
+    /// Reference the record.
+    pub fn rec(&self) -> &Record {
+        &self.rec
     }
 
     /// Reference the central material.
     pub fn mat(&self) -> &Material {
         &self.mat
+    }
+
+    /// Check if the cell contains intersecting triangles.
+    pub fn is_empty(&self) -> bool {
+        self.ent_tris.is_empty()
+    }
+
+    /// Determine the material at the given position within the cell.
+    #[pre(dom.contains(&p))]
+    #[pre(self.aabb.contains(&p))]
+    #[pre(!ents.is_empty())]
+    pub fn mat_at_pos(&self, p: &Point3<f64>, dom: &Aabb, ents: &'a Vec<Entity>) -> &'a Material {
+        mat_at_pos_from_sublist(p.clone(), dom, ents, &self.aabb, &self.ent_tris)
+    }
+
+    /// Determine the distance to an entity contained within the cell.
+    pub fn ent_dist(&self, ray: &Ray) -> Option<(f64, &'a Entity<'a>)> {
+        let mut closest: Option<(f64, &'a Entity<'a>)> = None;
+
+        for (ent, tris) in self.ent_tris.iter() {
+            for tri in tris {
+                if let Some(dist) = tri.dist(ray) {
+                    if closest.is_none() || (dist < closest.unwrap().0) {
+                        closest = Some((dist, ent));
+                    }
+                }
+            }
+        }
+
+        closest
+    }
+
+    /// Determine the distance to an entity, and the corresponding collision normal, contained within the cell.
+    pub fn ent_dist_norm(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>, &'a Entity<'a>)> {
+        let mut closest: Option<(f64, Unit<Vector3<f64>>, &'a Entity<'a>)> = None;
+
+        for (ent, tris) in self.ent_tris.iter() {
+            for tri in tris {
+                if let Some((dist, norm)) = tri.dist_norm(ray) {
+                    if closest.is_none() || (dist < closest.unwrap().0) {
+                        closest = Some((dist, norm, ent));
+                    }
+                }
+            }
+        }
+
+        closest
     }
 
     /// Add a record to this cell's record.
--- src/sim/mcrt.rs
+++ src/sim/mcrt.rs
@@ -5,6 +5,8 @@ use crate::{
     dom::Cell,
     index::bin,
     opt::Photon,
+    rng::henyey_greenstein,
+    rt::{Gate, Traceable},
     util::progress::bar,
     world::{Light, Universe},
 };
@@ -12,9 +14,12 @@ use contracts::pre;
 use indicatif::ProgressBar;
 use log::info;
 use rand::rngs::ThreadRng;
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
+use std::{
+    f64::consts::PI,
+    sync::{Arc, Mutex},
+};
 
 /// Distance to bump over boundaries to prevent getting stuck.
 const BUMP_DIST: f64 = 1.0e-9;
@@ -98,8 +103,8 @@ fn iterate(
 }
 
 fn run_photon(
-    archive: &mut Archive,
-    rng: &mut ThreadRng,
+    mut archive: &mut Archive,
+    mut rng: &mut ThreadRng,
     total_phot: u64,
     light: &Light,
     uni: &Universe,
@@ -107,8 +112,72 @@ fn run_photon(
     let mut phot = light.emit(&mut rng, total_phot);
     let mut cell_rec = cell_and_record(&phot, uni, &mut archive);
     cell_rec.1.increase_emissions(phot.weight());
-    let mut env = cell_rec.0.mat_at_pos(&phot.ray().pos).env(&phot);
+    let mut env = cell_rec
+        .0
+        .mat_at_pos(&phot.ray().pos, uni.grid().aabb(), uni.ents())
+        .env(phot.wavelength());
     let mut shifted = false;
+
+    loop {
+        let inter_dist = -(rng.gen_range(0.0f64, 1.0)).ln() / env.inter_coeff;
+        let cell_dist = cell_rec.0.aabb().dist(phot.ray()).unwrap();
+        let ent_info = cell_rec.0.ent_dist(phot.ray());
+
+        match HitEvent::new(inter_dist, cell_dist, ent_info) {
+            HitEvent::Scattering { dist } => {
+                cell_rec.1.increase_scatters(phot.weight());
+
+                phot.travel(dist);
+                cell_rec.1.increase_dist_travelled(dist);
+
+                phot.rotate(
+                    henyey_greenstein(&mut rng, env.asym),
+                    rng.gen_range(0.0, 2.0 * PI),
+                );
+
+                cell_rec.1.increase_absorptions(env.albedo * phot.weight());
+                phot.multiply_weight(env.albedo);
+
+                if !shifted && rng.gen_range(0.0, 1.0) <= env.shift_prob {
+                    shifted = true;
+                }
+            }
+            HitEvent::Boundary { dist } => {
+                phot.travel(dist + BUMP_DIST);
+                cell_rec.1.increase_dist_travelled(dist + BUMP_DIST);
+
+                if !uni.grid().aabb().contains(&phot.ray().pos) {
+                    break;
+                }
+
+                cell_rec = cell_and_record(&phot, uni, &mut archive);
+            }
+            HitEvent::Entity { dist: _ } => {
+                let (ent, dist, norm) = cell_rec.0.ent_dist_norm(phot.ray()).unwrap();
+                let inside = phot.ray().dir.dot(&norm) > 0.0;
+
+                let next_mat = if inside { ent.out_mat() } else { ent.in_mat() };
+                let next_env = next_mat.env(&phot);
+
+                let n_curr = env.ref_index;
+                let n_next = next_env.ref_index;
+
+                let gate = Gate::new(&phot.ray().dir, &norm, n_curr, n_next);
+
+                if rng.gen_range(0.0, 1.0) <= gate.ref_prob() {
+                    phot.travel(dist - BUMP_DIST);
+                    cell_rec.1.increase_dist_travelled(dist - BUMP_DIST);
+                    phot.set_dir(*gate.ref_dir());
+                } else {
+                    phot.travel(dist + BUMP_DIST);
+                    cell_rec.1.increase_dist_travelled(dist + BUMP_DIST);
+                    phot.set_dir(gate.trans_dir().unwrap());
+
+                    env = next_env;
+                }
+            }
+        }
+    }
 }
 
 /// Retrieve a reference for the cell corresponding record a photon is located within.
@@ -122,4 +191,66 @@ fn cell_and_record<'a>(
     let ans = (&uni.grid().cells()[index], &mut archive.recs[index]);
 
     ans
+}
+
+/// Hit event types.
+enum HitEvent {
+    /// Scattering event.
+    Scattering {
+        /// Distance to the scattering event.
+        dist: f64,
+    },
+    /// Cell boundary collision.
+    Boundary {
+        /// Distance to the cell surface.
+        dist: f64,
+    },
+    /// Entity surface collision.
+    Entity {
+        /// Distance to the entity surface.
+        dist: f64,
+    },
+}
+
+impl HitEvent {
+    #[pre(dist > 0.0)]
+    pub fn new_scattering(dist: f64) -> Self {
+        Self::Scattering { dist }
+    }
+
+    #[pre(dist > 0.0)]
+    pub fn new_boundary(dist: f64) -> Self {
+        Self::Boundary { dist }
+    }
+
+    #[pre(dist > 0.0)]
+    pub fn new_entity(dist: f64) -> Self {
+        Self::Entity { dist }
+    }
+
+    #[pre(inter_dist > 0.0)]
+    #[pre(cell_dist > 0.0)]
+    pub fn new(inter_dist: f64, cell_dist: f64, ent_dist: Option<f64>) -> Self {
+        if cell_dist <= inter_dist {
+            if let Some(ent_dist) = ent_dist {
+                if ent_dist < cell_dist {
+                    if (ent_dist - cell_dist).abs() <= BUMP_DIST {
+                        panic!("Entity and cell on close approach!");
+                    }
+
+                    return Self::new_entity(ent_dist);
+                }
+            }
+
+            return Self::new_boundary(cell_dist);
+        }
+
+        if let Some(ent_dist) = ent_dist {
+            if ent_dist < (inter_dist + BUMP_DIST) {
+                return Self::new_entity(ent_dist);
+            }
+        }
+
+        Self::new_scattering(inter_dist)
+    }
 }
