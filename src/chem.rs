5cfc93a398692f2d5747869923b6aa3b5fa55da0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-14 00:05:31 +0000

    Moved reaction to form submodule.

--- src/chem.rs
+++ src/chem.rs
@@ -1,7 +1,5 @@
 //! Chemical structures.
 
 pub mod molecule;
-pub mod reaction;
 
 pub use self::molecule::*;
-pub use self::reaction::*;
--- src/form.rs
+++ src/form.rs
@@ -2,10 +2,12 @@
 
 pub mod entity;
 pub mod mcrt;
+pub mod reaction;
 pub mod setup;
 pub mod transform;
 
 pub use self::entity::*;
 pub use self::mcrt::*;
+pub use self::reaction::*;
 pub use self::setup::*;
 pub use self::transform::*;
