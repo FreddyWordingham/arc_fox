e02a8967b4cead545db10db8196d903921ccbd6a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-14 00:00:43 +0000

    Completed reaction fields.

--- src/chem/reaction.rs
+++ src/chem/reaction.rs
@@ -9,14 +9,32 @@ pub struct Reaction {
     id: String,
     /// Rate.
     rate: f64,
+    /// Reactants and their relative ratios.
+    reactants: Vec<(String, i32)>,
+    /// Products and their relative ratios.
+    products: Vec<(String, i32)>,
 }
 
 impl Reaction {
     #[pre(!id.is_empty())]
     #[pre(rate > 0.0)]
+    #[pre(!reactants.is_empty())]
+    #[pre(reactants.iter().all(|(_id, n)| *n > 0))]
+    #[pre(!products.is_empty())]
+    #[pre(products.iter().all(|(_id, n)| *n > 0))]
     /// Construct a new instance.
-    pub fn new(id: String, rate: f64) -> Self {
-        Self { id, rate }
+    pub fn new(
+        id: String,
+        rate: f64,
+        reactants: Vec<(String, i32)>,
+        products: Vec<(String, i32)>,
+    ) -> Self {
+        Self {
+            id,
+            rate,
+            products,
+            reactants,
+        }
     }
 }
 
