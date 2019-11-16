0488428024c4968ecf1b124e255ace5abcb02423
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-16 23:32:31 +0000

    Improved setup struct.

--- src/form/setup.rs
+++ src/form/setup.rs
@@ -1,6 +1,12 @@
 //! Setup structure.
 
-use crate::{base::Resolution, geom::shape::ProtoMesh, json, mat::ProtoInterface};
+use crate::{
+    base::Resolution,
+    geom::{shape::ProtoMesh, ProtoTransform},
+    json,
+    mat::ProtoInterface,
+};
+use nalgebra::Translation3;
 use serde::{Deserialize, Serialize};
 
 /// Setup structure implementation.
@@ -18,11 +24,37 @@ impl Setup {
     pub fn example() -> Self {
         Self {
             res: Resolution::new(9, 9, 9),
-            inters: vec![ProtoInterface::new(
-                ProtoMesh::new("torus".to_string(), None),
-                "fog".to_string(),
-                "air".to_string(),
-            )],
+            inters: vec![
+                ProtoInterface::new(
+                    ProtoMesh::new(
+                        "plane".to_string(),
+                        Some(ProtoTransform::new(
+                            Some(Translation3::new(0.0, 0.0, 0.75)),
+                            None,
+                            Some(1.05),
+                        )),
+                    ),
+                    "fog".to_string(),
+                    "air".to_string(),
+                ),
+                ProtoInterface::new(
+                    ProtoMesh::new("torus".to_string(), None),
+                    "fog".to_string(),
+                    "air".to_string(),
+                ),
+                ProtoInterface::new(
+                    ProtoMesh::new(
+                        "plane".to_string(),
+                        Some(ProtoTransform::new(
+                            Some(Translation3::new(0.0, 0.0, -0.75)),
+                            None,
+                            Some(1.05),
+                        )),
+                    ),
+                    "air".to_string(),
+                    "fog".to_string(),
+                ),
+            ],
         }
     }
 }
