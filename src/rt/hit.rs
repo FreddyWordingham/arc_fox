da53ecfb675dd95bfd9d558bcee170e4c11995ac
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-17 01:07:14 +0000

    Added to the ray-tracing module.

--- src/rt/hit.rs
+++ src/rt/hit.rs
@@ -3,5 +3,12 @@
 /// Hit enumeration implementation.
 #[derive(Debug)]
 pub enum Hit {
-    // Variants.
+    /// Scattering event.
+    Scattering(f64),
+    /// Cell collision.
+    Cell(f64),
+    /// Interface collision.
+    Interface(f64),
+    /// Interface collision, followed by a close cell collision.
+    InterfaceCell(f64),
 }
