598e199b8ef310f027f6121a391d5470f0d72b23
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-17 00:53:28 +0000

    Added data structures.

--- src/data/archive.rs
+++ src/data/archive.rs
@@ -1,17 +1,29 @@
 //! Archive structure.
 
-use contracts::pre;
+use super::Record;
+use crate::base::Resolution;
+use ndarray::Array3;
+use std::ops::AddAssign;
 
 /// Archive structure implementation.
+/// Record archive.
 #[derive(Debug)]
 pub struct Archive {
-    // Fields.
+    /// Record array.
+    recs: Array3<Record>,
 }
 
 impl Archive {
     /// Construct a new instance.
-    #[pre(true)]
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(res: Resolution) -> Self {
+        Self {
+            recs: Array3::from_elem(res.arr, Record::new()),
+        }
+    }
+}
+
+impl AddAssign<&Archive> for Archive {
+    fn add_assign(&mut self, rhs: &Self) {
+        self.recs += &rhs.recs;
     }
 }
--- src/data/record.rs
+++ src/data/record.rs
@@ -1,17 +1,59 @@
 //! Record structure.
 
-use contracts::pre;
+use std::ops::{Add, AddAssign};
 
 /// Record structure implementation.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Record {
-    // Fields.
+    /// Total weight of photon emissions.
+    emissions: f64,
+    /// Total weight of scattering events.
+    scatters: f64,
+    /// Total weight of absorption events.
+    absorptions: f64,
+    /// Total distance travelled by photons.
+    dist_travelled: f64,
 }
 
 impl Record {
     /// Construct a new instance.
-    #[pre(true)]
     pub fn new() -> Self {
-        Self {}
+        Self {
+            emissions: 0.0,
+            scatters: 0.0,
+            absorptions: 0.0,
+            dist_travelled: 0.0,
+        }
+    }
+}
+
+impl Add<&Self> for Record {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self {
+        Self {
+            emissions: self.emissions + rhs.emissions,
+            scatters: self.scatters + rhs.scatters,
+            absorptions: self.absorptions + rhs.absorptions,
+            dist_travelled: self.dist_travelled + rhs.dist_travelled,
+        }
+    }
+}
+
+impl AddAssign for Record {
+    fn add_assign(&mut self, rhs: Self) {
+        self.emissions += rhs.emissions;
+        self.scatters += rhs.scatters;
+        self.absorptions += rhs.absorptions;
+        self.dist_travelled += rhs.dist_travelled;
+    }
+}
+
+impl AddAssign<&Self> for Record {
+    fn add_assign(&mut self, rhs: &Self) {
+        self.emissions += rhs.emissions;
+        self.scatters += rhs.scatters;
+        self.absorptions += rhs.absorptions;
+        self.dist_travelled += rhs.dist_travelled;
     }
 }
