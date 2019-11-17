d6768b3a6ca276130287a9bb836fbe6274a3cb57
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-17 00:43:54 +0000

    Added utility structures.

--- src/util/range.rs
+++ src/util/range.rs
@@ -1,17 +1,73 @@
 //! Range structure.
 
-use contracts::pre;
+use contracts::{post, pre};
+use std::f64::{INFINITY, NEG_INFINITY};
 
 /// Range structure implementation.
+/// One-dimensional inclusive Range.
 #[derive(Debug)]
 pub struct Range {
-    // Fields.
+    /// Minimum bound.
+    min: f64,
+    /// Maximum bound.
+    max: f64,
 }
 
 impl Range {
     /// Construct a new instance.
-    #[pre(true)]
-    pub fn new() -> Self {
-        Self {}
+    #[pre(min < max)]
+    pub fn new(min: f64, max: f64) -> Self {
+        Self { min, max }
+    }
+
+    /// Construct an infinite range.
+    pub fn new_infinite() -> Self {
+        Self::new(NEG_INFINITY, INFINITY)
+    }
+
+    /// Get the minimum bound.
+    pub fn min(&self) -> f64 {
+        self.min
+    }
+
+    /// Get the maximum bound.
+    pub fn max(&self) -> f64 {
+        self.max
+    }
+
+    /// Calculate the width of the Range.
+    #[post(ret > 0.0)]
+    pub fn width(&self) -> f64 {
+        self.max - self.min
+    }
+
+    /// Determine if a value is contained within the Range.
+    pub fn contains(&self, x: f64) -> bool {
+        if x < self.min || x > self.max {
+            return false;
+        }
+
+        true
+    }
+
+    /// Determine if the Range intersects with another given Range.
+    pub fn intersect(&self, other: &Self) -> bool {
+        if self.max < other.min || other.max < self.min {
+            return false;
+        }
+
+        true
+    }
+
+    /// Form a range of overlapping values.
+    pub fn overlap(&self, other: &Self) -> Option<Self> {
+        if !self.intersect(other) {
+            return None;
+        }
+
+        let min = self.min.max(other.min);
+        let max = self.max.min(other.max);
+
+        Some(Self::new(min, max))
     }
 }
--- src/util/tag.rs
+++ src/util/tag.rs
@@ -1,17 +1,65 @@
 //! Tag structure.
 
-use contracts::pre;
+use std::cmp::{Ordering, PartialEq, PartialOrd};
 
 /// Tag structure implementation.
-#[derive(Debug)]
-pub struct Tag {
-    // Fields.
+/// Sorts according to the label.
+#[derive(Debug, Clone, Copy)]
+pub struct Tag<T, S> {
+    /// Sorting label.
+    pub label: T,
+    /// Stored value.
+    pub value: S,
 }
 
-impl Tag {
+impl<T: Copy + PartialEq + PartialOrd, S: Copy> Tag<T, S> {
     /// Construct a new instance.
-    #[pre(true)]
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(label: T, value: S) -> Self {
+        Self { label, value }
+    }
+
+    /// Destruct the object into its component label and value pair.
+    pub fn components(self) -> (T, S) {
+        (self.label, self.value)
+    }
+
+    /// Determine the minimum and return a copy.
+    pub fn min(&self, other: &Self) -> Self {
+        if self.label < other.label {
+            return *self;
+        }
+        *other
+    }
+
+    /// Determine the maximum and return a copy.
+    pub fn max(&self, other: &Self) -> Self {
+        if self.label > other.label {
+            return *self;
+        }
+        *other
+    }
+}
+
+impl<T: PartialOrd, S> PartialEq for Tag<T, S> {
+    fn eq(&self, other: &Self) -> bool {
+        self.label == other.label
+    }
+}
+
+impl<T: PartialOrd, S> PartialOrd for Tag<T, S> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.label.partial_cmp(&other.label)
+    }
+}
+
+impl<T: PartialOrd, S> PartialEq<T> for Tag<T, S> {
+    fn eq(&self, other: &T) -> bool {
+        self.label == *other
+    }
+}
+
+impl<T: PartialOrd, S> PartialOrd<T> for Tag<T, S> {
+    fn partial_cmp(&self, other: &T) -> Option<Ordering> {
+        self.label.partial_cmp(&other)
     }
 }
