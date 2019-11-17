9f1187b296a75e3d02b1ad051544bcf547d3e3ff
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-17 00:44:35 +0000

    Added progress bar function.

--- src/util/progress.rs
+++ src/util/progress.rs
@@ -1,1 +1,18 @@
 //! Progress functions.
+
+use indicatif::{ProgressBar, ProgressStyle};
+
+/// Construct a progress bar with a default style.
+pub fn bar(msg: &'static str, ticks: u64) -> ProgressBar {
+    let bar = ProgressBar::new(ticks);
+    bar.set_style(
+            ProgressStyle::default_bar()
+                .template(
+                    "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] [{pos}/{len}] {percent}% ({eta}) {msg}",
+                )
+                .progress_chars("#>-"),
+        );
+    bar.set_message(msg);
+
+    bar
+}
