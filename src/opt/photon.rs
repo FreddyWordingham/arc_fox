6559d262ee5e5f7d1a90addcf2e1c021c53d4c0b
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-17 01:16:21 +0000

    Added to optical module.

--- src/opt/photon.rs
+++ src/opt/photon.rs
@@ -1,17 +1,56 @@
 //! Photon structure.
 
-use contracts::pre;
+use crate::rt::Ray;
+use contracts::{post, pre};
 
 /// Photon structure implementation.
 #[derive(Debug)]
 pub struct Photon {
-    // Fields.
+    /// Statistical weight.
+    weight: f64,
+    /// Wavelength [m].
+    wavelength: f64,
+    /// Power [J/s].
+    power: f64,
+    /// Ray of travel.
+    ray: Ray,
 }
 
 impl Photon {
     /// Construct a new instance.
-    #[pre(true)]
-    pub fn new() -> Self {
-        Self {}
+    #[pre(wavelength > 0.0)]
+    #[pre(power > 0.0)]
+    pub fn new(wavelength: f64, power: f64, ray: Ray) -> Self {
+        Self {
+            weight: 1.0,
+            wavelength,
+            power,
+            ray,
+        }
+    }
+
+    /// Get the weight.
+    #[post(ret > 0.0)]
+    #[post(ret <= 1.0)]
+    pub fn weight(&self) -> f64 {
+        self.weight
+    }
+
+    /// Get the wavelength.
+    #[post(ret > 0.0)]
+    pub fn wavelength(&self) -> f64 {
+        self.wavelength
+    }
+
+    /// Get the power.
+    #[post(ret > 0.0)]
+    pub fn power(&self) -> f64 {
+        self.power
+    }
+
+    /// Reference the ray.
+    #[post((ret.dir().magnitude() - 1.0).abs() < 1.0e-6)]
+    pub fn ray(&self) -> &Ray {
+        &self.ray
     }
 }
