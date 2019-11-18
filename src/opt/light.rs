7b6d2ff21e4ae265d3674df24f9c118cb8a95759
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-18 03:14:44 +0000

    Added lights.

--- src/opt/light.rs
+++ src/opt/light.rs
@@ -1,17 +1,35 @@
 //! Light structure.
 
+use super::{Photon, Spectrum};
+use crate::rt::Emit;
 use contracts::pre;
+use rand::rngs::ThreadRng;
 
 /// Light structure implementation.
 #[derive(Debug)]
 pub struct Light {
-    // Fields.
+    /// Emission surface.
+    surf: Box<dyn Emit>,
+    /// Emission spectrum.
+    spec: Spectrum,
+    /// Power. [J/s]
+    power: f64,
 }
 
 impl Light {
     /// Construct a new instance.
-    #[pre(true)]
-    pub fn new() -> Self {
-        Self {}
+    #[pre(power > 0.0)]
+    pub fn new(surf: Box<dyn Emit>, spec: Spectrum, power: f64) -> Self {
+        Self { surf, spec, power }
+    }
+
+    /// Emit a new photon.
+    #[pre(total_phot > 0)]
+    pub fn emit(&self, rng: &mut ThreadRng, total_phot: u64) -> Photon {
+        Photon::new(
+            self.spec.sample(rng),
+            self.power / total_phot as f64,
+            self.surf.emit(rng),
+        )
     }
 }
