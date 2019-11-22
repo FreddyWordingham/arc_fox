ea10241fb5d9dcad45faa1758d934c868b8db5ad
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-22 00:38:00 +0000

    Added datacube pack saving.

--- src/file/save.rs
+++ src/file/save.rs
@@ -22,7 +22,7 @@ pub fn as_json<T: Serialize>(obj: &T, path: &Path) {
 }
 
 impl<T: Debug + Numeric> Save for Array3<T> {
-    #[pre(self.shape().iter().all(|x| *x > 0))]
+    #[pre(self.len() > 0)]
     fn save(&self, path: &Path) {
         let mut file = File::create(&path).expect("Unable to create file.");
 
@@ -51,5 +51,45 @@ impl<T: Debug + Numeric> Save for Array3<T> {
             None,
         )
         .expect("Unable to store datacube values.");
+    }
+}
+
+impl<T: Debug + Numeric> Save for Vec<(String, Array3<T>)> {
+    #[pre(self.len() > 0)]
+    #[pre(self.iter().all(|(name, arr)| !name.is_empty() && arr.len() > 0))]
+    fn save(&self, path: &Path) {
+        let mut file = File::create(&path).expect("Unable to create file.");
+
+        let shape = self[0].1.shape();
+
+        let dim1_name = "x";
+        file.root_mut()
+            .add_dimension(dim1_name, shape[X as usize])
+            .expect("Unable to add X dimension.");
+        let dim2_name = "y";
+        file.root_mut()
+            .add_dimension(dim2_name, shape[Y as usize])
+            .expect("Unable to add Y dimension.");
+        let dim3_name = "z";
+        file.root_mut()
+            .add_dimension(dim3_name, shape[Z as usize])
+            .expect("Unable to add Z dimension.");
+
+        for (name, arr) in self.iter() {
+            if arr.shape() != shape {
+                panic!("Unable to save datacube pack, shapes do not match.");
+            }
+
+            let var = &mut file
+                .root_mut()
+                .add_variable::<T>(name, &[dim1_name, dim2_name, dim3_name])
+                .expect("Unable to add datacube entry.");
+            var.put_values(
+                arr.as_slice().expect("Unable to write datacube values."),
+                None,
+                None,
+            )
+            .expect("Unable to store datacube values.");
+        }
     }
 }
