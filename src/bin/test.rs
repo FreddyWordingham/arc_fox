29723631dbb580586914677ceba751b64d9d49aa
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-11-29 14:20:21 +0000

    Added simple scatter plotter.

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,5 +1,25 @@
 //! Test function.
 
+use arc::sci::math::{
+    rt::{Emit, Trace},
+    shape::Aabb,
+};
+use nalgebra::{Point3, Vector3};
+use rand::thread_rng;
+
 fn main() {
-    println!("Hello World!");
+    let source = Point3::origin();
+    let mut rng = thread_rng();
+    let dom = Aabb::new_centred(&source, &Vector3::new(1.0, 1.0, 1.0));
+    for _n in 0..1000 {
+        let ray = source.emit(&mut rng);
+        let hp = dom.hit_point(&ray).unwrap();
+
+        println!(
+            "{:<32}{:<32}{:<32}",
+            format!("{},", hp.x),
+            format!("{},", hp.y),
+            hp.z
+        );
+    }
 }
