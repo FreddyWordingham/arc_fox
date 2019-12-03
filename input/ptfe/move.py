f8d35ea86572a892a2fad527b6d7cfa9d0c5205a
Laura Moran <lm579@exeter.ac.uk>
2019-12-03 14:45:10 +0000

    Python script in ptfe section to edit setup.json in order to change the x axis location of the ptfe slab.

---
+++ input/ptfe/move.py
@@ -0,0 +1,21 @@
+import json
+import sys
+import numpy as np
+from collections import OrderedDict
+
+with open("setup.json","r") as jsonFile:
+    data = json.load(jsonFile, object_pairs_hook=OrderedDict)
+
+tmp = data["uni"]
+map = tmp["inter_map"]
+slab = map["slab"]
+mesh = slab["mesh"]
+outerTrans = mesh["trans"]
+innerTrans = outerTrans["trans"]
+#print(innerTrans)
+innerTrans[0] = np.float(sys.argv[1])
+#print(innerTrans)
+#print(outerTrans)
+print("data: ", data)
+with open("setup.json", "w") as jsonFile:
+    json.dump(data, jsonFile)
