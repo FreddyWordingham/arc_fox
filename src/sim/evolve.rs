c911d18d9285bd0731a6ecb440c5e01cf3c89c3e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2019-12-18 12:33:28 +0000

    Added hidden files.

--- src/sim/evolve.rs
+++ src/sim/evolve.rs
@@ -9,18 +9,24 @@ use contracts::pre;
 /// Run a time evolution simulation.
 #[pre(num_threads > 0)]
 #[pre(time > 0.0)]
-pub fn run(num_threads: usize, time: f64, _universe: &mut Universe) {
-    // let mut mults = universe
-    //     .grid()
-    //     .cells()
-    //     .map(|cell| cell.mat().reaction_multiplier());
+pub fn run(num_threads: usize, time: f64, universe: &mut Universe) {
+    let mut mults = universe
+        .grid()
+        .cells()
+        .map(|cell| cell.mat().reaction_multiplier());
 
     // let reactions = universe.reactions();
     // universe
-    //     .grid_mut()
-    //     .cells_mut()
-    //     .into_par_iter()
-    //     .for_each(|cell| evolve_cell(cell, reactions, time));
+    // .grid_mut()
+    // .cells_mut()
+    // .into_par_iter()
+    // .for_each(|cell| evolve_cell(cell, reactions, time));
+
+    for (cell, mult) in universe.grid_mut().cells_mut().zip(mults.iter()) {
+        // evolve_cell(cell, reactions, time);
+        let state = cell.state_mut();
+        for reaction in universe.reactions() {}
+    }
 }
 
 /// Evolve a single cell.
