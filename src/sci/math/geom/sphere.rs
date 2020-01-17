e0787e41499337ffbef55d484cfed6e7c4116171
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-17 17:17:29 +0000

    Added trace trait to sphere struct.

--- src/sci/math/geom/sphere.rs
+++ src/sci/math/geom/sphere.rs
@@ -4,7 +4,7 @@ use crate::sci::math::{
     geom::{Aabb, Collide},
     rt::{Ray, Trace},
 };
-use nalgebra::{Point3, Vector3};
+use nalgebra::{Point3, Unit, Vector3};
 use std::f64::consts::PI;
 
 /// Sphere geometry.
@@ -44,6 +44,26 @@ impl Sphere {
     pub fn contains(&self, p: &Point3<f64>) -> bool {
         nalgebra::distance_squared(&self.pos, p) <= self.rad.powi(2)
     }
+
+    /// Determine the intersection distances along a ray's direction.
+    #[must_use]
+    fn intersections(&self, ray: &Ray) -> Option<(f64, f64)> {
+        let m = ray.pos - self.pos;
+        let b = m.dot(&ray.dir);
+        let c = (m.dot(&m)) - self.rad.powi(2);
+
+        if c > 0.0 && b > 0.0 {
+            return None;
+        }
+
+        let disc = b.powi(2) - c;
+        if disc <= 0.0 {
+            return None;
+        }
+
+        let disc_sqrt = disc.sqrt();
+        Some((-b - disc_sqrt, -b + disc_sqrt))
+    }
 }
 
 impl Collide for Sphere {
@@ -57,5 +77,61 @@ impl Collide for Sphere {
     #[must_use]
     fn overlap(&self, aabb: &Aabb) -> bool {
         aabb.dist_sq_from_point(&self.pos) <= self.rad.powi(2)
+    }
+}
+
+impl Trace for Sphere {
+    #[must_use]
+    fn hit(&self, ray: &Ray) -> bool {
+        self.intersections(ray).is_some()
+    }
+
+    #[must_use]
+    fn dist(&self, ray: &Ray) -> Option<f64> {
+        if let Some((min, max)) = self.intersections(ray) {
+            if min > 0.0 {
+                return Some(min);
+            } else if max > 0.0 {
+                return Some(max);
+            }
+        }
+
+        None
+    }
+
+    #[must_use]
+    fn dist_norm(&self, ray: &Ray) -> Option<(f64, Unit<Vector3<f64>>)> {
+        if let Some(dist) = self.dist(ray) {
+            let mut hit = ray.clone();
+            hit.travel(dist);
+            return Some((dist, Unit::new_normalize(hit.pos - self.pos)));
+        }
+
+        None
+    }
+
+    #[must_use]
+    fn dist_inside(&self, ray: &Ray) -> Option<(f64, bool)> {
+        if let Some((min, max)) = self.intersections(ray) {
+            if min > 0.0 {
+                return Some((min, false));
+            }
+            if max > 0.0 {
+                return Some((max, true));
+            }
+        }
+
+        None
+    }
+
+    #[must_use]
+    fn dist_inside_norm(&self, ray: &Ray) -> Option<(f64, bool, Unit<Vector3<f64>>)> {
+        if let Some((dist, inside)) = self.dist_inside(ray) {
+            let mut hit = ray.clone();
+            hit.travel(dist);
+            return Some((dist, inside, Unit::new_normalize(hit.pos - self.pos)));
+        }
+
+        None
     }
 }
