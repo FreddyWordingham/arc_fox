af1b95297ac9bba606bd84b1ada25e6cf2960775
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-20 15:07:11 +0000

    Implemented load trait for triangle and smooth triangle structs.

--- src/sci/math/geom/smooth_triangle.rs
+++ src/sci/math/geom/smooth_triangle.rs
@@ -2,13 +2,20 @@
 
 use crate::{
     access,
+    file::io::Load,
     sci::math::{
         geom::{Aabb, Collide, Triangle},
         rt::{Ray, Trace},
     },
     util::list::alphabet::Greek::{Alpha, Beta, Gamma},
 };
-use nalgebra::{Unit, Vector3};
+use nalgebra::{Point3, Unit, Vector3};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+    result::Result,
+};
 
 /// Triangle structure implementation with Phong normal interpolation.
 /// Forms meshes.
@@ -102,5 +109,89 @@ impl Trace for SmoothTriangle {
         } else {
             None
         }
+    }
+}
+
+impl Load for Vec<SmoothTriangle> {
+    fn load(path: &Path) -> Self {
+        let vertex_lines: Vec<_> = BufReader::new(File::open(path).expect("Unable to open file!"))
+            .lines()
+            .map(Result::unwrap)
+            .filter(|line| line.starts_with("v "))
+            .collect();
+
+        let mut verts = Vec::with_capacity(vertex_lines.len());
+        for line in vertex_lines {
+            let mut words = line.split_whitespace();
+            words.next();
+
+            let px = words
+                .next()
+                .expect("Could not get the next word.")
+                .parse::<f64>()
+                .expect("Could not parse px value.");
+            let py = words
+                .next()
+                .expect("Could not get the next word.")
+                .parse::<f64>()
+                .expect("Could not parse py value.");
+            let pz = words
+                .next()
+                .expect("Could not get the next word.")
+                .parse::<f64>()
+                .expect("Could not parse pz value.");
+
+            verts.push(Point3::new(px, py, pz));
+        }
+
+        let normal_lines: Vec<_> = BufReader::new(File::open(path).expect("Unable to open file!"))
+            .lines()
+            .map(Result::unwrap)
+            .filter(|line| line.starts_with("vn "))
+            .collect();
+
+        let mut norms = Vec::with_capacity(normal_lines.len());
+        for line in normal_lines {
+            let mut words = line.split_whitespace();
+            words.next();
+
+            let nx = words.next().unwrap().parse::<f64>().unwrap();
+            let ny = words.next().unwrap().parse::<f64>().unwrap();
+            let nz = words.next().unwrap().parse::<f64>().unwrap();
+
+            norms.push(Unit::new_normalize(Vector3::new(nx, ny, nz)));
+        }
+
+        let face_lines: Vec<_> = BufReader::new(File::open(path).expect("Unable to open file!"))
+            .lines()
+            .map(Result::unwrap)
+            .filter(|line| line.starts_with("f "))
+            .collect();
+
+        let mut faces = Vec::with_capacity(face_lines.len());
+        for line in face_lines {
+            let line = line.replace("//", " ");
+            let mut words = line.split_whitespace();
+            words.next();
+
+            let fx = words.next().unwrap().parse::<usize>().unwrap() - 1;
+            let nx = words.next().unwrap().parse::<usize>().unwrap() - 1;
+            let fy = words.next().unwrap().parse::<usize>().unwrap() - 1;
+            let ny = words.next().unwrap().parse::<usize>().unwrap() - 1;
+            let fz = words.next().unwrap().parse::<usize>().unwrap() - 1;
+            let nz = words.next().unwrap().parse::<usize>().unwrap() - 1;
+
+            faces.push(((fx, fy, fz), (nx, ny, nz)));
+        }
+
+        let mut tris = Vec::with_capacity(faces.len());
+        for face in faces {
+            tris.push(SmoothTriangle::new(
+                Triangle::new([verts[(face.0).0], verts[(face.0).1], verts[(face.0).2]]),
+                [norms[(face.1).0], norms[(face.1).1], norms[(face.1).2]],
+            ));
+        }
+
+        tris
     }
 }
