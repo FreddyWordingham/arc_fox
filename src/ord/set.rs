069f57ca1d91e1ac47f80ef8cb509a1e0452ad69
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-25 22:27:20 +0000

    Added requirement functions.

--- src/ord/set.rs
+++ src/ord/set.rs
@@ -1,6 +1,11 @@
 //! Set storage map structure.
 
-use crate::{access, file::io::Load, ord::Name, sci::chem::Reaction};
+use crate::{
+    access,
+    file::io::Load,
+    ord::Name,
+    sci::{chem::Reaction, phys::Interface},
+};
 use log::info;
 use std::{collections::BTreeMap, ops::Index, path::Path};
 
@@ -49,6 +54,9 @@ impl<T> Index<&str> for Set<T> {
     }
 }
 
+/// Get a list of all species required for the set of reactions.
+#[inline]
+#[must_use]
 pub fn req_species(reactions: &Set<Reaction>) -> Vec<String> {
     let mut species = Vec::new();
 
@@ -60,4 +68,37 @@ pub fn req_species(reactions: &Set<Reaction>) -> Vec<String> {
     species.dedup();
 
     species
+}
+
+/// Get a list of all meshes required for the set of interfaces.
+#[inline]
+#[must_use]
+pub fn req_meshes(interfaces: &Set<Interface>) -> Vec<String> {
+    let mut meshes = Vec::new();
+
+    for i in interfaces.map().values() {
+        meshes.push(i.surf().clone());
+    }
+
+    meshes.sort();
+    meshes.dedup();
+
+    meshes
+}
+
+/// Get a list of all materials required for the set of interfaces.
+#[inline]
+#[must_use]
+pub fn req_materials(interfaces: &Set<Interface>) -> Vec<String> {
+    let mut materials = Vec::new();
+
+    for i in interfaces.map().values() {
+        materials.push(i.in_mat().clone());
+        materials.push(i.out_mat().clone());
+    }
+
+    materials.sort();
+    materials.dedup();
+
+    materials
 }
