d2a882d5e981a337dbbbeb115e7edffb4f4c5d17
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-28 13:50:01 +0000

    Added python plotting script.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Library core.
+
+#![warn(
+    clippy::all,
+    // clippy::cargo
+    clippy::missing_docs_in_private_items,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::restriction,
+)]
+#![allow(
+    clippy::implicit_return,
+    clippy::integer_arithmetic,
+    clippy::module_name_repetitions,
+    clippy::panic,
+    clippy::result_expect_used,
+    clippy::wildcard_enum_match_arm
+)]
