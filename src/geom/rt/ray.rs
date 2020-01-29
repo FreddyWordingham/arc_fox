a878298ff792b55b60a3c7604d4ce8d1c08d892e
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-29 12:03:06 +0000

    Added photon and ray structures.

--- src/geom/rt/ray.rs
+++ src/geom/rt/ray.rs
@@ -1,1 +1,49 @@
 //! Ray implementation.
+
+use crate::access;
+use nalgebra::{Point3, Rotation3, Unit, Vector3};
+
+/// Ray structure.
+#[derive(Debug, Clone)]
+pub struct Ray {
+    /// Ray origin.
+    pos: Point3<f64>,
+    /// Ray direction.
+    dir: Unit<Vector3<f64>>,
+}
+
+impl Ray {
+    access!(pos, Point3<f64>);
+    access!(dir, dir_mut, Unit<Vector3<f64>>);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub const fn new(pos: Point3<f64>, dir: Unit<Vector3<f64>>) -> Self {
+        Self { pos, dir }
+    }
+
+    /// Move along the direction of travel a given distance.
+    #[inline]
+    pub fn travel(&mut self, dist: f64) {
+        self.pos += self.dir.as_ref() * dist;
+    }
+
+    /// Rotate the photon with a given pitch and subsequent roll manoeuvre.
+    #[inline]
+    pub fn rotate(&mut self, pitch: f64, roll: f64) {
+        let arbitrary_axis = if (self.dir.z.abs() - 1.0) >= 1.0e-1 {
+            Vector3::z_axis()
+        } else {
+            Vector3::y_axis()
+        };
+
+        let pitch_axis = Unit::new_normalize(self.dir.cross(&arbitrary_axis));
+        let pitch_rot = Rotation3::from_axis_angle(&pitch_axis, pitch);
+
+        let roll_rot = Rotation3::from_axis_angle(&self.dir, roll);
+
+        self.dir = roll_rot * pitch_rot * self.dir;
+        self.dir.renormalize();
+    }
+}
--- src/phys/part/photon.rs
+++ src/phys/part/photon.rs
@@ -1,1 +1,35 @@
 //! Photon implementation.
+
+use crate::{access, geom::Ray};
+
+/// Photon structure.
+#[derive(Debug)]
+pub struct Photon {
+    /// Statistical weight.
+    weight: f64,
+    /// Wavelength [m].
+    wavelength: f64,
+    /// Power [J/s].
+    power: f64,
+    /// Ray of travel.
+    ray: Ray,
+}
+
+impl Photon {
+    access!(weight, f64);
+    access!(wavelength, f64);
+    access!(power, f64);
+    access!(ray, ray_mut, Ray);
+
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub const fn new(wavelength: f64, power: f64, ray: Ray) -> Self {
+        Self {
+            weight: 1.0,
+            wavelength,
+            power,
+            ray,
+        }
+    }
+}
