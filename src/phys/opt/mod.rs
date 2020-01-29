90f110502add2e9fc40a66be843422c76366dd69
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-01-29 14:53:12 +0000

    Added material and optics structs.

--- src/phys/opt/mod.rs
+++ src/phys/opt/mod.rs
@@ -2,6 +2,7 @@
 
 pub mod crossing;
 pub mod environment;
+pub mod optics;
 pub mod spectrum;
 
-pub use self::{crossing::*, environment::*, spectrum::*};
+pub use self::{crossing::*, environment::*, optics::*, spectrum::*};
