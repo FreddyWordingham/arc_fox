24583a574a2878108020d1fb96c5051368e3f4e0
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-01 23:27:03 +0000

    Added hit enumeration.

---
+++ src/sim/mcrt/hit.rs
@@ -0,0 +1,39 @@
+//! Hit-type enumeration.
+
+/// Hit enumeration implementation.
+#[derive(Debug)]
+pub enum Hit {
+    /// Scattering event.
+    Scattering(f64),
+    /// Cell collision.
+    Cell(f64),
+    /// Interface collision.
+    Interface(f64),
+    /// Interface collision, followed by a close cell collision.
+    InterfaceCell(f64),
+}
+
+impl Hit {
+    /// Construct a new instance.
+    pub fn new(scat_dist: f64, cell_dist: f64, inter_dist: Option<f64>, bump_dist: f64) -> Self {
+        if cell_dist <= scat_dist {
+            if let Some(inter_dist) = inter_dist {
+                if cell_dist <= (inter_dist + (bump_dist * 1.0)) {
+                    return Self::InterfaceCell(inter_dist);
+                }
+
+                return Self::InterfaceCell(inter_dist);
+            }
+
+            return Self::Cell(cell_dist);
+        }
+
+        if let Some(inter_dist) = inter_dist {
+            if inter_dist <= scat_dist {
+                return Self::Interface(inter_dist);
+            }
+        }
+
+        Self::Scattering(scat_dist)
+    }
+}
