06869aa52b938d36cd8310d6725d0ded591ee1bb
Freddy Wordingham <fjmw201@exeter.ac.uk>
2020-02-01 15:04:11 +0000

    Added state struct.

--- src/uni/part/state.rs
+++ src/uni/part/state.rs
@@ -1,1 +1,24 @@
 //! State implementation.
+
+use crate::access;
+use ndarray::Array1;
+
+/// Local species state.
+pub struct State {
+    /// Species concentrations.
+    concs: Array1<f64>,
+    /// Species sources.
+    sources: Array1<f64>,
+}
+
+impl State {
+    access!(concs, concs_mut, Array1<f64>);
+    access!(sources, Array1<f64>);
+
+    /// Construct  a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(concs: Array1<f64>, sources: Array1<f64>) -> Self {
+        Self { concs, sources }
+    }
+}
